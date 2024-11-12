856e1c1f50c0ab0412e3f51ecd0c5e0ec9f86042
Multichoiceagency <139119345+Multichoiceagency@users.noreply.github.com>
2024-11-12 11:26:54 +0100

    Commit new

--- utils/apollo-client.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://atahome.multichoiceagency.nl/graphql', // Vervang met je WordPress GraphQL endpoint
-  cache: new InMemoryCache(),
-});
-
-export default client;
